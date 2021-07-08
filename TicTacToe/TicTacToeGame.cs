307be23f3884d106d45ff2953909b18df8758f10
antoniu.pitic <antoniu.pitic@gmail.com>
2021-07-08 11:17:48 +0300

    - Move at x,y - Set winner if WeHaveAWinner - not the best solution

--- TicTacToe/TicTacToeGame.cs
+++ TicTacToe/TicTacToeGame.cs
@@ -6,6 +6,7 @@ namespace TicTacToe
     {
         int[,] board = new int[Ct.BoardSize, Ct.BoardSize];
         int currentPlayer;
+        int winner;
         public void Start()
         {
             InitBoard();
@@ -14,14 +15,33 @@ namespace TicTacToe
 
         public void Move(int x, int y)
         {
-            if (board[x,y] == Ct.FreeCell)
+            if (board[x, y] == Ct.FreeCell)
             {
                 board[x, y] = currentPlayer;
-                TogglePlayer(ref currentPlayer);
+                if (WeHaveAWinner(x, y))
+                {
+                    winner = currentPlayer;
+                }
+                else
+                {
+                    TogglePlayer();
+                }
+
             }
         }
 
-        private void TogglePlayer(ref int currentPlayer)
+        private bool WeHaveAWinner(int x, int y)
+        {
+            if (board[x, 0] == board[x, 1] && board[x, 1] == board[x, 2]) return true;
+            if (board[0, y] == board[1, y] && board[1, y] == board[2, y]) return true;
+
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2]) return true;
+            if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0]) return true;
+
+            return false;
+        }
+
+        private void TogglePlayer()
         {
             currentPlayer = (currentPlayer == Ct.FirstPlayer) ? Ct.SecondPlayer : Ct.FirstPlayer;
         }
